2682bced0c31be940095cfd7e305dbbc10654c0b
Marlou Lopez <08.marlou.lopez@gmail.com>
2019-01-15 09:56:51 +0800

    Initial Commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+   require_once 'core/init.php';
+   include 'templates/header.php';
+
+   if(Session::exists('home')){
+      echo Session::flash('home');
+   }
+
+   $user = new User();
+
+   if($user->isLoggedIn()){
+      ?>
+         <p>Hello <a href="http:profile.php?user=<?php echo escape($user->data()->username); ?>"><?php echo escape($user->data()->username); ?></a></p>
+
+         <ul>
+            <li><a href="http:logout.php">Log out</a></li>
+            <li><a href="http:update.php">Update Details</a></li>
+            <li><a href="http:changepassword.php">Change Password</a></li>
+         </ul>
+      <?php
+
+      if($user->hasPermission('admin')){
+         echo 'You are an admin';
+      }
+
+   } else{
+      echo '<p>You need to <a href="login.php">log in</a> or <a href="register.php">register</a></p>';
+   }
+
+   include 'templates/footer.php';
